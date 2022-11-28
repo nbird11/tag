c9eb385eeb8cac0cafe0132f5b1923702718c303
Nathan Bird <bir19004@byui.edu>
2022-11-28 09:52:04 -0700

    add Director object code

---
+++ Game/Directing/Director.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using Tag.Game.Services;
+using Tag.Game.Casting;
+using Tag.Game.Scripting;
+
+namespace Tag.Game.Directing
+{
+    /// <summary> The responsibility of Director is to direct all of
+    /// the actors in their separate functions and behaviors.
+    /// <para> Control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private VideoService _videoService = null;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given VideoService.
+        /// </summary>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(VideoService videoService)
+        {
+            this._videoService = videoService;
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast and script.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        /// <param name="script">The given script.</param>
+        public void StartGame(Cast cast, Script script)
+        {
+            _videoService.OpenWindow();
+            while (_videoService.IsWindowOpen())
+            {
+                ExecuteActions("input", cast, script);
+                ExecuteActions("update", cast, script);
+                ExecuteActions("output", cast, script);
+            }
+            _videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Calls execute for each action in the given group.
+        /// </summary>
+        /// <param name="group">The group name.</param>
+        /// <param name="cast">The cast of actors.</param>
+        /// <param name="script">The script of actions.</param>
+        private void ExecuteActions(string group, Cast cast, Script script)
+        {
+            List<Action> actions = script.GetActions(group);
+            foreach(Action action in actions)
+            {
+                action.Execute(cast, script);
+            }
+        }
+    }
+}
