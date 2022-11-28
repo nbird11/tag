e793d13b4bd1f3c4352a9a79734bf9fa55704658
Nathan Bird <bir19004@byui.edu>
2022-11-28 10:03:13 -0700

    skeleton structure for Program Main

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Tag
+﻿using System.Collections.Generic;
+using Tag.Game.Casting;
+using Tag.Game.Directing;
+using Tag.Game.Scripting;
+using Tag.Game.Services;
+
+namespace Tag
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            // ADD CAST
+            Cast cast = new Cast();
+
+            // ADD SERVICES
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+
+            // ADD SCRIPTS
+            Script script = new Script();
+
+            // ADD DIRECTOR
+            Director director = new Director(videoService);
             
+            // Start Game
+            director.StartGame(cast, script);
         }
     }
 }
