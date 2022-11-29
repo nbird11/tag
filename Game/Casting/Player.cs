3eef85a7d820258128a603bf81e99b9b43eb0c73
Jakedez <jakedez7@gmail.com>
2022-11-29 10:07:43 -0700

    Minor Changes to Player class. Created collision class.

--- Game/Casting/Player.cs
+++ Game/Casting/Player.cs
@@ -1,16 +1,17 @@
 using System;
+using Raylib_cs;
 
 namespace Tag.Game.Casting
 {
     public class Player : Actor
     {
         // ATTRIBUTES
-        private Actor player = new Actor();
-
+        private Point _oldPos = new Point(0, 0);
+        private Point _size = new Point(0, 0);
         // CONTRUCTOR
         public Player()
         {
-
+            
         }
 
         // METHODS
