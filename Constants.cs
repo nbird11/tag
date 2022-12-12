53cfbd61fb06a872567c7c897f1a4a236009f1e0
Jakedez <jakedez7@gmail.com>
2022-12-12 09:06:03 -0700

    changed cell size and frame rate

--- Constants.cs
+++ Constants.cs
@@ -10,11 +10,11 @@ namespace Tag
     {
         public static int COLUMNS = 40;
         public static int ROWS = 20;
-        public static int CELL_SIZE = 15;
+        public static int CELL_SIZE = 30;
         public static int MAX_X = 900;
         public static int MAX_Y = 600;
 
-        public static int FRAME_RATE = 15;
+        public static int FRAME_RATE = 60;
         public static int FONT_SIZE = 15;
         public static string CAPTION = "Tag";
 
